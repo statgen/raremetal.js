b6f71b9b1049e615853fbc7470f73ce3cc877b1c
Ryan Welch <welchr@umich.edu>
2018-04-10 18:12:28 -0400

    First stab at code to parse portal JSON

--- src/app/browser.js
+++ src/app/browser.js
@@ -8,7 +8,144 @@
  * @license MIT
  */
 
-const hail = require("./hail.js");
-const stats = require("./stats.js");
+const {VariantMask, ScoreStatTable, GenotypeCovarianceMatrix, testBurden, testSkat} = require("./stats.js");
+const fetch = require("node-fetch");
 
-module.exports = { hail, stats };
+/**
+ * Check if we're running in node.js or browser
+ * Borrows code from https://github.com/iliakan/detect-node (MIT)
+ * @return {boolean}
+ */
+function isNode() {
+  return Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]';
+}
+
+async function parsePortalJson(json) {
+  // Load the masks
+  let masks = {};
+  for (let maskData of json.data.masks) {
+    let mask = new VariantMask();
+    mask.id = maskData.id;
+    mask.label = maskData.label;
+
+    for (let [group, groupData] of Object.entries(maskData.groups)) {
+      mask.createGroup(group,groupData.variants);
+    }
+
+    masks[mask.id] = mask;
+  }
+
+  // Load scores and covariance matrices
+  let loaded = [];
+  for (let scoreBlock of json.scorecov) {
+    let variants = masks[scoreBlock.mask].getGroup(scoreBlock.group);
+    let positions = variants.map(x => parseInt(x.match(/(chr)?(\w+):(\d+)_([A-Z]+)\/([A-Z]+)/)[3]));
+
+    // Scores
+    let scoreTable = new ScoreStatTable();
+    scoreTable.sampleSize = scoreBlock.nsamples;
+    for (let i = 0; i < scoreBlock.scores.length; i++) {
+      // Diagonal element of linearized (lower triangular) covariance matrix
+      let n = i + 1;
+      let variance = scoreBlock.covariance[n*(n+1)/2-1];
+
+      scoreTable.appendScore(
+        variants[i],
+        positions[i],
+        scoreBlock.scores[i],
+        variance,
+        scoreBlock.alt_freq[i]
+      );
+    }
+
+    // Preallocate matrix
+    const n_variants = variants.length;
+    let covmat = new Array(n_variants);
+    for (let i = 0; i < n_variants; i++) {
+      covmat[i] = new Array(n_variants).fill(null);
+    }
+
+    // Map from variant ID or position => matrix index
+    const posMap = new Map();
+    for (let i = 0; i < n_variants; i++) {
+      let vobj = variants[i];
+      posMap.set(vobj.pos, i);
+    }
+
+    // Load the covariance matrix from the response JSON
+    let c = 0;
+    for (let i = 0; i < n_variants; i++) {
+      for (let j = 0; j < i + 1; j++) {
+        covmat[i][j] = scoreBlock.covariance[c];
+        covmat[j][i] = scoreBlock.covariance[c];
+        c += 1;
+      }
+    }
+
+    // Construct the covariance matrix object and store it
+    let covMatrix = new GenotypeCovarianceMatrix(covmat,variants,posMap);
+
+    // Store result
+    loaded.push({
+      mask: scoreBlock.mask,
+      group: scoreBlock.group,
+      nsamples: scoreBlock.nsamples,
+      scores: scoreTable,
+      covariance: covMatrix
+    });
+  }
+
+  return loaded;
+}
+
+/**
+ * Example of running many tests + masks at once
+ * @return {Promise<void>}
+ * @private
+ */
+async function _example() {
+  // Load example JSON of portal response from requesting covariance in a region
+  let json;
+  if (isNode()) {
+    const fs = require("fs");
+    let jsonRaw = fs.readFileSync("example.json");
+    json = JSON.parse(jsonRaw);
+  }
+  else {
+    const response = await fetch("example.json");
+    json = await response.json();
+  }
+
+  return parsePortalJson(json);
+
+  // Run all tests/masks
+  /*
+  let results = await runAggregationTests(
+    {
+      "Collapsing burden test": testBurden,
+      "SKAT test": testSkat,
+    },
+    {
+      "PTV": {
+        mask: mask1,
+        label: "Only protein truncating variants",
+        type: "gene"
+      },
+      "LOF": {
+        mask: mask2,
+        label: "Loss of function variants",
+        type: "gene"
+      }
+    },
+    score_cov,
+    {
+      id: 100, // This gets repeated in the response
+      description: "This is an example of running multiple tests and masks at once"
+    }
+  );
+
+  return results;
+  */
+}
+
+module.exports = {parsePortalJson, _example};
