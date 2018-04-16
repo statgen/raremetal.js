e75fb9662051b1934fa0e5e635e7985ba62800be
Ryan Welch <welchr@umich.edu>
2018-04-16 12:56:03 -0400

    Test case for testBurden

---
+++ test/unit/stats.js
@@ -0,0 +1,21 @@
+const {ScoreStatTable, testBurden} = require("../../src/app/stats.js");
+
+describe("stats.js", function() {
+  describe("testBurden", function() {
+    it("should return correct p-value for known u/cov (no weights)", function() {
+      let u = [ 1.26175, 3.45806, -4.90216, -7.05748 ];
+      let cov = [ [ 23.902543, -0.01359241884, -0.01361261692, -0.1976943976 ],
+        [ -0.01359241884, 23.90577896, -0.01371627432, -0.1992892636 ],
+        [ -0.01361261692, -0.01371627432, 23.9075214, -0.1996334844 ],
+        [ -0.1976943976, -0.1992892636, -0.1996334844, 320.2882088 ] ];
+      let [, pval] = testBurden(u,cov,null);
+      let expectedPval = 0.7141;
+      assert.closeTo(
+        pval,
+        expectedPval,
+        0.001,
+        "testBurden on known u/cov did not produce close enough p-value to expected"
+      )
+    })
+  })
+});
