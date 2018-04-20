0a84d472debe1431248b50d221c7a5fcd9d5fd7d
Ryan Welch <welchr@umich.edu>
2018-04-20 16:34:16 -0400

    Lint fix

--- test/integration/run-aggregation-tests.js
+++ test/integration/run-aggregation-tests.js
@@ -1,9 +1,9 @@
-const {parsePortalJson, runAggregationTests} = require("../../src/app/browser.js");
-const {testBurden, testSkat} = require("../../src/app/stats.js");
-const fs = require("fs");
+const {parsePortalJson, runAggregationTests} = require('../../src/app/browser.js');
+const {testBurden, testSkat} = require('../../src/app/stats.js');
+const fs = require('fs');
 
-describe("Full integration of covariance and aggregation tests", function() {
-  describe("runAggregationTests()", function() {
+describe('Full integration of covariance and aggregation tests', function() {
+  describe('runAggregationTests()', function() {
     let results;
 
     before(function() {
@@ -11,24 +11,24 @@ describe("Full integration of covariance and aggregation tests", function() {
       this.timeout(10000);
 
       // Load example JSON of portal response from requesting covariance in a region
-      let jsonRaw = fs.readFileSync("test/integration/example.json");
+      let jsonRaw = fs.readFileSync('test/integration/example.json');
       let json = JSON.parse(jsonRaw);
       var scoreCov = parsePortalJson(json);
       // Run all tests/masks and return results
       results = runAggregationTests(
         {
-          "zegginiBurden": testBurden,
-          "skat": testSkat
+          'zegginiBurden': testBurden,
+          'skat': testSkat
         },
         scoreCov,
         {
           id: 100, // This gets repeated in the response
-          description: "This is an example of running multiple tests and masks at once"
+          description: 'This is an example of running multiple tests and masks at once'
         }
       );
     });
 
-    it("should match expected burden p-value for HIC2", function() {
+    it('should match expected burden p-value for HIC2', function() {
       let t = results.data.results.filter(x => x.group === 'HIC2' && x.test === 'zegginiBurden')[0];
       assert.closeTo(t.pvalue,0.42913956,0.001);
     });
