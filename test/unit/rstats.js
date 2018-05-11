10fb9fd395ec2fbf0ed909b172dc5d71c4690f4e
Ryan Welch <welchr@umich.edu>
2018-05-11 16:30:08 -0400

    Test cases for pnorm

--- test/unit/rstats.js
+++ test/unit/rstats.js
@@ -1,7 +1,8 @@
-import { pchisq, dbeta } from '../../src/app/rstats.js';
+import { dbeta, pchisq, pnorm } from '../../src/app/rstats.js';
 import { assert } from 'chai';
 import * as yaml from 'js-yaml';
 import fs from 'fs';
+import sqlite3 from 'sqlite3';
 
 function* range(start, end, increment=1) {
   for (let i = start; i < end; i += increment) {
@@ -141,13 +142,92 @@ describe('rstats.js', function() {
       let tests = yaml.safeLoad(fs.readFileSync('test/unit/dbeta.yaml', 'utf8'));
       for (let t of tests) {
         let actual = dbeta(t.x, t.a, t.b, yamlParseBoolean(t.give_log));
-        if (!isFinite(t.x) || !isFinite(t.expected)) {
+        if (isNaN(t.x)) { assert.isNaN(actual) }
+        else if (!isFinite(t.x) || !isFinite(t.expected)) {
           assert.equal(actual, t.expected, `failed on x=${t.x}, a=${t.a}, b=${t.b}, log.p=${t.give_log}`);
         }
         else {
           assert(nearlyEqual(actual, t.expected, 0.001), `failed on x=${t.x}, a=${t.a}, b=${t.b}, log.p=${t.give_log}`);
         }
       }
+    });
+  });
+
+  describe('pnorm()', function() {
+    it('should match expected values from d-p-q-r-tests.R', function () {
+      let x = [-Infinity, -1e100, ...range(1,7), 1e200, Infinity];
+      let exp = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1];
+      x.forEach((v, i) => {
+        let actual = pnorm(v, 3, 0);
+        assert.equal(actual, exp[i], `pnorm(${v},3,0) expected ${exp[i]} got ${actual}`);
+      });
+
+      let exp2 = [0, ...Array(8).fill(0.5), 1];
+      x.forEach((v, i) => {
+        let actual = pnorm(v, 3, Infinity);
+        assert.equal(actual, exp2[i], `pnorm(${v},3,Inf) expected ${exp2[i]} got ${actual}`)
+      });
+
+      let z = [1.2616,0.1422,1.2383,-0.75,0.7788,-0.8034,0.1592,2.046,1.0922,-0.0049,0.9458,-1.3031,-0.2147,0.6323,
+        0.4443,0.5156,-0.9995,1.5754,-1.6227,-0.0779,1.8652,0.5917,0.497,-1.227,-1.067,1.1307,1.4506,0.3941,0.1166,
+        -1.2651,0.3122,-0.7621,0.4518,0.3998,0.8039,0.5097,-1.4709,-0.4076,-0.3053,0.9183,-1.7226,-0.3332,-0.2315,
+        0.3279,-0.2472,1.0795,-0.3598,0.4343,0.0497,0.7131,1.1903,-0.1625,0.0497,0.1496,0.9787,0.926,0.4562,-0.3203,
+        0.949,-0.0795,1.3487,0.0824,0.4419,0.8125,1.296,1.9026,-0.4583,0.0374,0.6252,1.5999,-0.6404,0.29,1.1315,1.7466,
+        2.8616,-0.1085,-0.0022,1.0252,1.8164,0.79,0.2236,0.0955,-0.0526,0.1262,0.2345,1.1784,-0.6072,2.1711,-0.8096,
+        -0.3959,-0.3017,0.8944,-2.6268,1.8283,0.0046,0.4118,0.9103,-0.5463,-1.3732,-0.3442];
+
+      z.forEach((v, i) => {
+        let a = pnorm(v);
+        let aLog = Math.log(a);
+        let b = 1 - pnorm(-v);
+        let c = 1 - pnorm(v, 0, 1, false, false);
+        let d = pnorm(v, 0, 1, true, true);
+        assert(nearlyEqual(a, b));
+        assert(nearlyEqual(a, c));
+        assert(nearlyEqual(aLog, d));
+      });
+
+      let y = [...range(1,51), ...[...range(3,11), 20, 50, 150, 250].map(x => 10 ** x)];
+      y = [...y.map(x => -x), 0, ...y];
+      for (let b of [true, false]) {
+        y.forEach(v => {
+          assert(nearlyEqual(
+            pnorm(-v, 0, 1, true, b),
+            pnorm(+v, 0, 1, false, b)
+          ))
+        });
+      }
+    });
+
+    it('should match expected values over a range of parameters', function(done) {
+      // Edge cases
+      assert.equal(pnorm(Infinity), 1);
+      assert.equal(pnorm(-Infinity), 0);
+      assert(nearlyEqual(pnorm(1e154, 0, 1, false, true), -5e307));
+
+      // General parameters
+      let db = new sqlite3.Database('test/unit/pnorm.db');
+      db.all("SELECT * FROM PNORM", function(err, rows) {
+        if (rows.length === 0) {
+          throw new Error("Retrieved 0 rows from sqlite3 database");
+        }
+
+        for (let t of rows) {
+          let x = parseFloat(t.x);
+          let expected = parseFloat(t.expected);
+          let actual = pnorm(x, t.mu, t.sd, t.tail, t.give_log);
+
+          if (isNaN(expected)) { assert.isNaN(actual) }
+          else if (!isFinite(x) || !isFinite(expected)) {
+            assert.equal(actual, expected, `failed on x=${x}, mu=${t.mu}, sd=${t.sd}, tail=${t.tail}, log=${t.give_log}, expected was ${expected}, got ${actual} instead`);
+          }
+          else {
+            assert(nearlyEqual(actual, expected), `failed on x=${x}, mu=${t.mu}, sd=${t.sd}, tail=${t.tail}, log=${t.give_log}, expected was ${expected}, got ${actual} instead`);
+          }
+        }
+
+        done();
+      });
     });
   });
 });
