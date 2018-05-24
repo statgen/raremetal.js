0f2c2b030749b71035542936a76e5e4c3d4d894c
Ryan Welch <welchr@umich.edu>
2018-05-24 09:28:01 -0400

    Unit test for SkatTest

--- test/unit/stats.js
+++ test/unit/stats.js
@@ -1,4 +1,4 @@
-import { ZegginiBurdenTest } from '../../src/app/stats.js';
+import { ZegginiBurdenTest, SkatTest } from '../../src/app/stats.js';
 
 describe('stats.js', function() {
   describe('ZegginiBurdenTest', function() {
@@ -18,5 +18,57 @@ describe('stats.js', function() {
         'testBurden on known u/cov did not produce close enough p-value to expected'
       )
     })
-  })
+  });
+
+  describe('SkatTest', function() {
+    it('should return correct p-value for known u/cov (standard weights)', function () {
+      let u = [
+        1.26175,
+        3.45806,
+        -4.90216,
+        -7.05748
+      ];
+      let cov = [
+        [
+          23.902543,
+          -0.01359241884,
+          -0.01361261692,
+          -0.1976943976
+        ],
+        [
+          -0.01359241884,
+          23.90577896,
+          -0.01371627432,
+          -0.1992892636
+        ],
+        [
+          -0.01361261692,
+          -0.01371627432,
+          23.9075214,
+          -0.1996334844
+        ],
+        [
+          -0.1976943976,
+          -0.1992892636,
+          -0.1996334844,
+          320.2882088
+        ]
+      ];
+      let mafs = [
+        0.000281496,
+        0.000283886,
+        0.000284308,
+        0.00412922
+      ];
+      let agg = new SkatTest();
+      let [, pval] = agg.test(u, cov, null, mafs);
+      let expectedPval = 0.8110;
+      assert.closeTo(
+        pval,
+        expectedPval,
+        0.001,
+        'SkatTest on known u/cov did not produce close enough p-value to expected'
+      )
+    });
+  });
 });
