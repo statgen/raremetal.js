5c4623f03533e5efa831b7920c4c6977e3bc7c40
Ryan Welch <welchr@umich.edu>
2018-06-17 19:53:38 -0400

    Add integration test for CLI

---
+++ test/integration/cli.js
@@ -0,0 +1,18 @@
+import { assert } from 'chai';
+import { single } from '../../src/app/cli.js';
+
+describe('cli.js', function() {
+  it('should work', async function() {
+    const args = {
+      cov: 'test/integration/data/gene.DUXAP8.cov.assoc.gz',
+      score: 'test/integration/data/gene.DUXAP8.scores.assoc.gz',
+      mask: 'test/integration/data/gene.DUXAP8.mask.tab',
+      test: 'skat-davies',
+      group: null
+    };
+
+    const result = await single(args);
+    assert(result.results.length === 1);
+    assert.closeTo(result.results[0].pvalue,8.11e-1,0.001);
+  });
+});
