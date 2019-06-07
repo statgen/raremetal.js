bc2e7c6d59e78af9c873e980d30186616dfdde85
Ryan Welch <welchr@umich.edu>
2019-06-07 11:18:12 -0400

    Cholesky decomposition

---
+++ src/app/linalg.js
@@ -0,0 +1,26 @@
+import numeric from 'numeric';
+
+/**
+ * Return the cholesky decomposition A = GG'. The matrix G is returned.
+ * @param A
+ * @return {*}
+ */
+export function cholesky(A) {
+  const n = A.length;
+  const G = numeric.rep([n, n], 0);
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= i; j++) {
+      let s = A[j][i];
+      for (let k = 0; k <= j-1; k++) {
+        s = s - G[j][k] * G[i][k];
+      }
+      if (j < i) {
+        G[i][j] = s / G[j][j];
+      }
+      else {
+        G[i][i] = Math.sqrt(s);
+      }
+    }
+  }
+  return G;
+}
---
+++ test/unit/linalg.js
@@ -0,0 +1,20 @@
+import { cholesky } from '../../src/app/linalg.js';
+import { assert } from 'chai';
+import * as numeric from 'numeric';
+
+describe('linalg.js', function() {
+  describe('cholesky', function() {
+    it('simple case', function() {
+      const A = [[4,12,-16],[12,37,-43],[-16,-43,98]];
+      const G = cholesky(A);
+      assert.deepStrictEqual(G, [[2,0,0],[6,1,0],[-8,5,3]]);
+    });
+
+    it('test when not perfect integers', function() {
+      const A = [[8, 2, 3], [2, 9, 3], [3, 3, 6]];
+      const G = cholesky(A);
+      const reA = numeric.dot(G, numeric.transpose(G));
+      assert.closeTo(reA[0][0], 8, 1e-6);
+    });
+  });
+});
