de4e3d69b02cded1e84fe191e68e9221dadf7c59
Ryan Welch <welchr@umich.edu>
2019-07-07 02:20:55 -0400

    We must comply with The Linter

--- test/unit/quadrature.js
+++ test/unit/quadrature.js
@@ -6,14 +6,14 @@ describe('quadrature.js', function() {
     it('exp(-t*t / 2) from 0 to 10', function () {
       let integ = new GaussKronrod(21, 15);
       let f = x => Math.exp(-x * x / 2);
-      let [result, error] = integ.integrate(f, 0, 10);
+      let [result, ] = integ.integrate(f, 0, 10);
       assert.closeTo(result, 1.25331, 0.0001);
     });
 
     it('x * sin(x) from -20 to 20', function () {
       let integ = new GaussKronrod(21, 15);
       let f = x => Math.sin(x) * x;
-      let [result, error] = integ.integrate(f, -20, 20);
+      let [result, ] = integ.integrate(f, -20, 20);
       assert.closeTo(result, -14.497, 0.001);
     });
   });
