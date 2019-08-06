a01128af21d615975fc3ef3698f5a291393e58f3
Ryan Welch <welchr@umich.edu>
2019-08-06 17:52:19 -0400

    Include license for quadrature

--- src/app/quadrature.js
+++ src/app/quadrature.js
@@ -1,3 +1,16 @@
+/**
+ * A port of Boost's Gauss-Kronrod quadrature procedure. Original license header is:
+ *
+ *   Copyright John Maddock 2017.
+ *   Copyright Nick Thompson 2017.
+ *   Use, modification and distribution are subject to the
+ *   Boost Software License, Version 1.0. (See accompanying file
+ *   LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * @module quadrature
+ * @license Boost
+ */
+
 const GAUSS_KRONROD_ABSCISSA = {
   21: [
     0.00000000000000000000000000000000000e+00,
